5bd5378c2f16d9507baf4314adfa682571bb3651
AF <67248717+aaf2017@users.noreply.github.com>
2020-10-03 23:53:57 -0400

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+
+export class Nav extends Component {
+    state = {
+        toggle: false
+    }
+
+    menuToggle = () =>{
+       this.setState({toggle: !this.state.toggle})
+    }
+    render() {
+        const {toggle} = this.state;
+        return (
+            <div>
+                <ul className={toggle? "toggle" : ""}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li className="close" onClick={this.menuToggle}>X</li>
+                </ul>
+                <div className="menu" onClick={this.menuToggle}>Menu</div>  
+            </div>
+        )
+    }
+}
+
+export default Nav
