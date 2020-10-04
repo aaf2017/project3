8ce3a62e352ff331212d5632d503b9d50ac5ea19
aaf2017 <67248717+annafateeva2017@users.noreply.github.com>
2020-10-04 00:25:14 -0400

    Updates

---
+++ precache-manifest.670adcb025354ef0a35775003bde43f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a14206ed27c1823053975f905294f26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ee1113e83636a7498fe",
+    "url": "/static/css/main.ea357b65.chunk.css"
+  },
+  {
+    "revision": "fb1bc3366c1e9b376b41",
+    "url": "/static/js/2.796c086d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.796c086d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee1113e83636a7498fe",
+    "url": "/static/js/main.908fbf83.chunk.js"
+  },
+  {
+    "revision": "16ae9854644794f32750",
+    "url": "/static/js/runtime-main.89434f27.js"
+  }
+]);
